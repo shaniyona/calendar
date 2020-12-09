18afb0c189ab75b773a8a5ce40de611a98c5c3b9
shaniyona <41207214+shaniyona@users.noreply.github.com>
2020-12-08 21:53:52 -0500

    added onChange handlers for form, and created event list

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,23 +1,24 @@
 import React, { Component } from 'react';
-import CalendarTile  from '../components/CalendarTile.js'
-import {MONTH_MODE, WEEK_MODE, DAY_MODE} from '../ViewMode.js'
-import EventModal  from './EventModal.js'
+import CalendarTile from '../components/CalendarTile.js'
+import { MONTH_MODE, WEEK_MODE, DAY_MODE } from '../ViewMode.js'
+import EventModal from './EventModal.js'
 import * as DateUtil from '../util/CalendarUtil.js';
 
 class Calendar extends Component {
     constructor(props) {
         super(props);
-        // this.state = {isShowingModal: false};
+
         this.state = this.setUpCalendar();
         this.state.isShowingModal = false;
+        this.state.eventList = [];
     }
 
     componentDidMount() {
         setInterval(() => {
-            this.setState({date: new Date()});
-        },5000);
+            this.setState({ date: new Date() });
+        }, 5000);
     }
-  
+
     componentWillUnmount() {
     }
 
@@ -28,51 +29,53 @@ class Calendar extends Component {
         return {
             date: date,
             firstDay: firstDay
+
         };
     }
 
     render() {
-        const totNumDays = DateUtil.calculateNumDays(this.state.firstDay.getFullYear(),this.state.firstDay.getMonth());
+        const totNumDays = DateUtil.calculateNumDays(this.state.firstDay.getFullYear(), this.state.firstDay.getMonth());
         const totNumWeeks = Math.ceil(totNumDays / 7);
         let isFirstWeek = true;
         const weeks = [];
 
         let modal;
-        
-        if(this.state.isShowingModal){
+
+        if (this.state.isShowingModal) {
             const minutes = this.state.date.getMinutes();
             const hours = this.state.date.getHours();
             const day = this.state.clickedTile.day;
             const month = DateUtil.monthToNum(this.state.clickedTile.month) + 1;
             const year = this.state.clickedTile.year;
 
-            const formattedMinutes = (minutes < 10) ? `0${minutes}` : minutes; 
-            const formattedHours = (hours < 10) ? `0${hours}` : hours; 
+            const formattedMinutes = (minutes < 10) ? `0${minutes}` : minutes;
+            const formattedHours = (hours < 10) ? `0${hours}` : hours;
             const formattedDay = (day < 10) ? `0${day}` : day;
             const formattedMonth = (month < 10) ? `0${month}` : month;
             const formattedDate = `${year}-${formattedMonth}-${formattedDay}`
             const formattedTime = `${formattedHours}:${formattedMinutes}`;
 
             modal = <EventModal clickedTile={this.state.clickedTile}
-                                time={formattedTime}
-                                date={formattedDate}
-                                onCloseHandler={this.modalCloseHandler}
+                time={formattedTime}
+                date={formattedDate}
+                onCloseHandler={this.modalCloseHandler}
+                addEvent={this.addEvent}
             />;
         }
 
         // start at 1 because firstday = 1, iterate by week by incrementing by 7
         let date = new Date(this.state.firstDay);
 
-         //setting first day of week as date
+        //setting first day of week as date
         let diff = 0 - date.getDay();
         date.setDate(date.getDate() + diff);
 
-        for(let i = 1; i <= totNumDays; i += 7) {
+        for (let i = 1; i <= totNumDays; i += 7) {
             const week = [];
             // iterate through days of week and populate week array
-            for(let j = 0; j < 7; j++){
+            for (let j = 0; j < 7; j++) {
                 let newDate = new Date(date);
-                week.push(<CalendarTile date={newDate} isFirstWeek={isFirstWeek}/>);
+                week.push(<CalendarTile date={newDate} isFirstWeek={isFirstWeek} />);
                 date.setDate(date.getDate() + 1);
             }
             isFirstWeek = false;
@@ -85,30 +88,36 @@ class Calendar extends Component {
                 {weeks.map(el => <div className="week">{el}</div>)}
                 {modal}
             </div>
-          );
+        );
     }
 
-    
-  onTileClick = (e) => {
-     if(e.target.classList.contains("modal") === false && this.state.isShowingModal === true){
-        this.setState({isShowingModal: false});
-        return;
-     } else if(e.target.classList.contains("modal") === true && this.state.isShowingModal === true){ 
-        return;
-     }
-    this.setState({
-        isShowingModal: true,
-        clickedTile: {
-            day: e.target.getAttribute("data-day"),
-            month: e.target.getAttribute("data-month"),
-            year: e.target.getAttribute("data-year")
-        }
-    });
-  }
 
-  modalCloseHandler = (e) => {
-      this.setState({isShowingModal: false});
-  }
+    onTileClick = (e) => {
+        if (e.target.classList.contains("modal") === false && this.state.isShowingModal === true) {
+            this.setState({ isShowingModal: false });
+            return;
+        } else if (e.target.classList.contains("modal") === true && this.state.isShowingModal === true) {
+            return;
+        }
+
+        this.setState({
+            isShowingModal: true,
+            clickedTile: {
+                day: e.target.getAttribute("data-day"),
+                month: e.target.getAttribute("data-month"),
+                year: e.target.getAttribute("data-year")
+            }
+        });
+    }
+
+    modalCloseHandler = (e) => {
+        this.setState({ isShowingModal: false });
+    }
+
+    addEvent = (event) => {
+        this.state.eventList.push(event);
+        this.setState({eventList: this.state.eventList});
+    }
 }
 
 export default Calendar;
--- src/components/EventModal.js
+++ src/components/EventModal.js
@@ -3,6 +3,12 @@ import React, { Component } from 'react';
 class EventModal extends Component {
     constructor(props){
         super(props);
+        this.state = {
+          title: '',
+          date: this.props.date,
+          time: this.props.time,
+          location: '' 
+        }
 
     }
   render() {
@@ -12,16 +18,37 @@ class EventModal extends Component {
             <span className="modal" onClick={this.props.onCloseHandler}>X</span>
           </div>
           <form className="modal eventModalForm">
-            <input className="modal" type="text" placeholder="Event title"></input>
-            <input className="modal" type="date" defaultValue={this.props.date}></input>
-            <input className="modal" type="time" defaultValue={this.props.time}></input>
-            <input className="modal" type="text" placeholder="Enter location"></input>
+            <input className="modal" type="text" placeholder="Event title" onChange={this.handleEventTitleChange} required="required"></input>
+            <input className="modal" type="date" defaultValue={this.props.date} onChange={this.handleDateChange}></input>
+            <input className="modal" type="time" defaultValue={this.props.time} onChange={this.handleTimeChange}></input>
+            <input className="modal" type="text" placeholder="Enter location" onChange={this.handleLocationChange}></input>
+            <div className="modal eventModalFormFooter">
+              <input className="modal" type="submit" onClick={this.onSaveModal} value="Save"></input>
+            </div>
           </form>
-          <div className="modal eventModalFormFooter">
-            <button className="modal" type="button" onClick={this.props.onCloseHandler}>Save</button>
-          </div>
         </div>
     )
+  }
+  onSaveModal = (e) => {
+    e.preventDefault();
+    this.props.addEvent(this.state);
+    this.props.onCloseHandler();
+  }
+
+  handleEventTitleChange = (e) => {
+    this.setState({title: e.target.value});
+  }
+
+  handleDateChange = (e) => {
+    this.setState({date: e.target.value});
+  }
+
+  handleTimeChange = (e) => {
+    this.setState({time: e.target.value});
+  }
+
+  handleLocationChange = (e) => {
+    this.setState({location: e.target.value});
   }
 }
 
